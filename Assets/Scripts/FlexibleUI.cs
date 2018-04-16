372f99a82961c8839e004fc0f3cbbfb33b5f6047
Scott Metoyer <scott.metoyer@ucr.edu>
2018-04-16 16:36:37 -0700

    Started working button system

---
+++ Assets/Scripts/FlexibleUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FlexibleUI : MonoBehaviour
+{
+
+    public FlexibleUIData skinData;
+
+    protected virtual void OnSkinUI()
+    {
+
+    }
+
+    public virtual void Awake()
+    {
+        OnSkinUI();
+    }
+
+    public virtual void Update()
+    {
+        // This shouldn't make it into production
+        if (Application.isEditor)
+        {
+            OnSkinUI();
+        }
+    }
+}
