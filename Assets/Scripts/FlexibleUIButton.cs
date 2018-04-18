6cab27c829fcda7b2288afe60128d30b2d0f0260
Scott Metoyer <scott.metoyer@ucr.edu>
2018-04-18 14:43:06 -0700

    More UI work

--- Assets/Scripts/FlexibleUIButton.cs
+++ Assets/Scripts/FlexibleUIButton.cs
@@ -6,6 +6,23 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 [RequireComponent(typeof(Image))]
 public class FlexibleUIButton : FlexibleUI {
+  
+    Image image;
+    Button button;
 
+    protected override void OnSkinUI()
+    {
+        base.OnSkinUI();
 
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+
+        button.transition = Selectable.Transition.SpriteSwap;
+        button.targetGraphic = image;
+
+        image.sprite = skinData.buttonSprite;
+        button.spriteState = skinData.buttonSpriteState;
+
+        image.type = Image.Type.Sliced;
+    }
 }
